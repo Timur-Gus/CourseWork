b441ec4e14954ad2c6f68f9db4f90cda32e180d7
Timur-Gus <timurka24478@gmail.com>
2023-11-14 15:04:34 +0300

    Задание базовой сложности

--- src/Employee.java
+++ src/Employee.java
@@ -2,11 +2,50 @@ public class Employee {
     String fio;
     int salary, id, dept;
     static int totalEmployee;
-    public Employee(String fio, int dept,int salary){
+
+    public Employee(String fio, int dept, int salary) {
         this.fio = fio;
         this.dept = dept;
         this.salary = salary;
         totalEmployee++;
         id = totalEmployee;
     }
+    public int getDept() {
+        return dept;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public static int getTotalEmployee() {
+        return totalEmployee;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+    public void setDept(int dept) {
+        this.dept = dept;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник: " + fio +
+                ". Отдел № " + dept +
+                ", зарплата = " + salary +
+                ", id: " + id;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
+            Employee[] employees = new Employee[10];
+            employees[0] = new Employee("Иванов Иван Иванович", 2, 42000);
+            employees[1] = new Employee("Козлов Виктор Сергеевич", 3, 50000);
+            employees[2] = new Employee("Семенов Евгений Александрович", 1, 35000);
+            employees[3] = new Employee("Иванов Иван Иванович", 2, 45000);
+            employees[4] = new Employee("Иванов Иван Иванович", 2, 45000);
+            employees[5] = new Employee("Иванов Иван Иванович", 2, 45000);
+            employees[6] = new Employee("Петров Сергей Семенович",4,70000);
+            employees[7] = new Employee("Иванов Иван Иванович", 2, 45000);
+            employees[8] = new Employee("Иванова Кристина Олеговна", 3, 50000);
+            employees[9] = new Employee("Иванов Иван Иванович", 2, 45000);
 
+            printAllEployees(employees);
+            System.out.println("Сумма затрат на зарпалты в месяц: " + totalMonthSalary(employees));
+            System.out.println("Средняя зарплата сотрудника: " + averageSalary(employees));
+            employeeMaxSalary(employees);
+            employeeMinSalary(employees);
+            printAllFio(employees);
+        }
+        public static void printAllEployees(Employee[] arr){
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
+        }
+        public static int totalMonthSalary(Employee[] arr){
+            int total = 0;
+            for (int i = 0; i < arr.length; i++) {
+                total += arr[i].getSalary();
+            }
+            return total;
+        }
+        public static void employeeMinSalary(Employee[] arr) {
+            int min = 0;
+            for (int i = 0; i < (arr.length - 1); i++) {
+                if (arr[min].getSalary() > arr[i].getSalary()) {
+                    min = i;
+                }
+            }
+            System.out.println("Сотрудник с минимальной зарплатой: " + arr[min].getFio());
+        }
+        public static void employeeMaxSalary(Employee[] arr) {
+            int max = 0;
+            for (int i = 0; i < (arr.length - 1); i++) {
+                if (arr[max].getSalary() < arr[i].getSalary()) {
+                    max = i;
+                }
+            }
+            System.out.println("Сотрудник с максимальной зарплатой: " + arr[max].getFio());
+        }
+        public static double averageSalary(Employee[] arr) {
+            return (double) totalMonthSalary(arr) / arr.length;
+        }
+        public static void printAllFio (Employee[] arr){
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i].getFio());
+            }
     }
 }
